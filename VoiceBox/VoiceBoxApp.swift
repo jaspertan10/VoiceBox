e62edcff7d2042b4b944857513b69f248c2699fb
Jasper Tan <jaspertan@Jaspers-MacBook-Air.local>
2025-04-03 19:49:24 -0700

    Initial commit

---
+++ VoiceBox/VoiceBoxApp.swift
@@ -0,0 +1,17 @@
+//
+//  VoiceBoxApp.swift
+//  VoiceBox
+//
+//  Created by Jasper Tan on 4/3/25.
+//
+
+import SwiftUI
+
+@main
+struct VoiceBoxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
